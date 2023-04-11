0f76dfaaf2fcbf64f9bd1ce748b87fe8ef02a262
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-04-11 14:40:33 +0400

    Update Q13 (NewLineEdit).py

--- Q13 (NewLineEdit).py
+++ Q13 (NewLineEdit).py
@@ -1,7 +1,5 @@
 def EditSourceCode(SourceCode): 
   LineNumber = int(input("Enter line number of code to edit: "))
-  while LineNumber == 0:
-    LineNumber = int(input("You cannot change the total number of lines. Please try again: "))
   print(SourceCode[LineNumber])
   Choice = EMPTY_STRING
   while Choice != "C":
