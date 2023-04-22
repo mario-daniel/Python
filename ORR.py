a0b03a14442140c9ad194c49d9297c8de858b9a4
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-04-22 13:01:11 +0400

    Create ORR.py

---
+++ ORR.py
@@ -0,0 +1,17 @@
+def ExecuteAND(Memory, Registers, Address):
+  BinaryACC = ConvertToBinary(Registers[ACC])
+  BinaryOperand = ConvertToBinary(Memory[Address].OperandValue)
+  Final = EMPTY_STRING
+  while len(BinaryACC) < 8:
+    BinaryACC = '0' + BinaryACC
+  while len(BinaryOperand) < 8:
+    BinaryOperand = '0' + BinaryOperand
+  for Bit in range(len(BinaryACC)):
+    if BinaryACC[Bit] == '1' or BinaryOperand[Bit] == '1':
+      Final = Final + "1"
+    else:
+      Final = Final + "0"
+  Registers[ACC] = ConvertToDecimal(Final)
+  if Registers[STATUS] == ConvertToDecimal("001"):
+    ReportRunTimeError("Overflow", Registers)
+  return Registers
