762be8085a2eb047c38d90a6edbdf29c11c06398
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-04-22 14:04:04 +0400

    Update LSL.py

--- LSL.py
+++ LSL.py
@@ -1,7 +1,11 @@
 def ExecuteLSL(Memory, Registers, Address):
   Binary = ConvertToBinary(Registers[ACC])
+  while len(Binary) < 7:
+    Binary = "0" + Binary
   for i in range(Memory[Address].OperandValue):
     Binary += "0"
+  if len(Binary) > 7:
+    Binary = Binary[Memory[Address].OperandValue:]
   Registers[ACC] = ConvertToDecimal(Binary)
   if Registers[STATUS] == ConvertToDecimal("001"):
     ReportRunTimeError("Overflow", Registers)
