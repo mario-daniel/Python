155eefb3b437f125cb354707f397f54d9a801687
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-04-22 13:00:25 +0400

    Update BGT.py

--- BGT.py
+++ BGT.py
@@ -2,6 +2,6 @@ def ExecuteBGT(Registers, Address):
     StatusRegister = ConvertToBinary(Registers[STATUS])
     FlagN = StatusRegister[1]
     FlagZ = StatusRegister[0]
-    if FlagN and FlagZ == "0":
+    if FlagN == "0" and FlagZ == "0":
         Registers[PC] = Address
     return Registers
