559faf02afcff0bccf3000d60606328efd1c7085
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-04-23 13:57:59 +0400

    Update LSR.py

--- LSR.py
+++ LSR.py
@@ -2,6 +2,4 @@ def ExecuteLSR(Memory, Registers, Address):
   Binary = ConvertToBinary(Registers[ACC])
   Binary = Binary[:-(Memory[Address].OperandValue)]
   Registers[ACC] = ConvertToDecimal(Binary)
-  if Registers[STATUS] == ConvertToDecimal("001"):
-    ReportRunTimeError("Overflow", Registers)
   return Registers
