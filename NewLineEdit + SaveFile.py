e185a84d9a7c6aa47d203f323bd5e5ab420d3df0
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-05-01 16:44:44 +0400

    Update NewLineEdit + SaveFile.py

--- NewLineEdit + SaveFile.py
+++ NewLineEdit + SaveFile.py
@@ -18,3 +18,21 @@ def EditSourceCode(SourceCode):
                 FileIn.write(str(SourceCode[i])+"\n")
         DisplaySourceCode(SourceCode)
     return SourceCode
+
+  def NewLineEdit(SourceCode, LineNumber):
+  Line = SourceCode[LineNumber]
+  Label = input("Enter Label or press enter: ").replace(" ", EMPTY_STRING)
+  OpCode = input("Enter OpCode or press enter: ").replace(" ", EMPTY_STRING)
+  Operand = input("Enter Operand or press enter: ").replace(" ", EMPTY_STRING)
+  Comment = input("Enter Comment or press enter: ").strip()
+  if "*" not in Comment and Comment != EMPTY_STRING:
+    Comment = "*" + Comment
+  while len(Label) != 6 and Label != EMPTY_STRING:
+    Label = " " + Label
+  while len(OpCode) != 4 and OpCode != EMPTY_STRING:
+    OpCode = " " + OpCode
+  if (Label and OpCode and Operand and Comment) == EMPTY_STRING:
+    print("\nThere will be no changes made to the Source Code.\n")
+  else:
+    Line = Label + " " + OpCode + " " + Operand + " " + Comment
+  return Line
