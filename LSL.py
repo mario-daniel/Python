db5cce4f5db4c9bc17b04e8ecb78f9d0c6c7770e
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-05-10 12:02:11 +0400

    Update LSL.py

--- LSL.py
+++ LSL.py
@@ -1,5 +1,6 @@
 def ExecuteLSL(Memory, Registers, Address):
   Value = ConvertToBinary(Registers[ACC] << Memory[Address].OperandValue)
-  Value = Value[(Memory[Address].OperandValue):]
+  if len(Value) > 7:
+    Value = Value[(len(Value) - 7):]
   Registers[ACC] = ConvertToDecimal(Value)
   return Registers
