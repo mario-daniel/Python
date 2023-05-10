cbcb2cb58d3e53c67d88ca106415956ca0298e53
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-05-10 12:32:22 +0400

    Update Stack Overflow Flag.py

--- Stack Overflow Flag.py
+++ Stack Overflow Flag.py
@@ -1,13 +1,10 @@
 def ExecuteJSR(Memory, Registers, Address, SourceCode):
-  while True:
-    StackPointer = Registers[TOS] - 1
-    if StackPointer <= int(SourceCode[0]):
-      ReportRunTimeError("Stack Overflow", Registers)
-      break
-    else:
-      Memory[StackPointer].OperandValue = Registers[PC] 
-      Registers[PC] = Address 
-      Registers[TOS] = StackPointer
-      DisplayStack(Memory, Registers)
-      break
+  StackPointer = Registers[TOS] - 1
+  if StackPointer <= int(SourceCode[0]):
+    ReportRunTimeError("Overflow", Registers)
+  else:
+    Memory[StackPointer].OperandValue = Registers[PC] 
+    Registers[PC] = Address 
+    Registers[TOS] = StackPointer
+    DisplayStack(Memory, Registers)
   return Memory, Registers
