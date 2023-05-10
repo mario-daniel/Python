b1dc3c5827609c0d3525f25404421083d4abd9cf
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-05-10 12:07:33 +0400

    Update LSR.py

--- LSR.py
+++ LSR.py
@@ -1,5 +1,7 @@
 def ExecuteLSR(Memory, Registers, Address):
   Value = ConvertToBinary(Registers[ACC])
   Value = Value[:-(Memory[Address].OperandValue)]
+  if Value == EMPTY_STRING:
+    Value = "0"
   Registers[ACC] = ConvertToDecimal(Value)
   return Registers
