b5e178f5376d4c4dc2ca3b6e8b81588ba2ad5a05
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-05-13 10:44:57 +0400

    Update LSR.py

--- LSR.py
+++ LSR.py
@@ -1,3 +1,5 @@
+#Right shifts the value in the accumulator by the amount of times of the operand value. Then slicing it to 7 bits as MAX_INT is 127.
+
 def ExecuteLSR(Memory, Registers, Address):
   Value = ConvertToBinary(Registers[ACC])
   Value = Value[:-(Memory[Address].OperandValue)]
