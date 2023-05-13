778f036b0d4926073a387a63771782ca84ea4a38
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-05-13 10:43:02 +0400

    Update Q14 (BLT).py

--- Q14 (BLT).py
+++ Q14 (BLT).py
@@ -1,3 +1,5 @@
+#Checks if the operand is lesser than the value in the accumulator.
+
 def ExecuteBLT(Registers, Address):
     StatusRegister = ConvertToBinary(Registers[STATUS])
     FlagN = StatusRegister[1]
