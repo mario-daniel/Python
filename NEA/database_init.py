e412e7a1ad40eb62c40840dde1e4ce5dbff2dabb
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2023-12-17 14:21:49 +0400

    Update database_init.py

--- NEA/database_init.py
+++ NEA/database_init.py
@@ -3,60 +3,27 @@ import sqlite3
 conn = sqlite3.connect("rfid")
 cursor = conn.cursor()
 
-#Activity Table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Activity (
-        activity_id CHAR(1),
-        activity VARCHAR(20),
-        PRIMARY KEY (activity_id)
-    );
-''')
-
 #Facility Table
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Facility (
-        facility_id CHAR(1),
-        activity_id CHAR(1),
-        available BOOLEAN,
-        PRIMARY KEY (facility_id),
-        FOREIGN KEY (activity_id) REFERENCES Activity(activity_id)
+        facility_id INTEGER,
+        facility_name VARCHAR(15),
+        booking_required BOOLEAN,
+        PRIMARY KEY (facility_id)
     );
 ''')
 
-#Teacher Table
+#User Table
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Teacher (
-        teacher_id INTEGER NOT NULL,
+    CREATE TABLE IF NOT EXISTS User (
+        user_id VARCHAR(10),
+        facility_id INTEGER,
+        first_name VARCHAR(20),
+        last_name VARCHAR(20),
         password VARCHAR(20),
-        first_name VARCHAR(15),
-        last_name VARCHAR(15),
-        facility_id CHAR(1),
-        PRIMARY KEY (teacher_id),
+        class_grade VARCHAR(3),
+        PRIMARY KEY (user_id),
         FOREIGN KEY (facility_id) REFERENCES Facility(facility_id)
-    );
-''')
-
-#Approval Table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Approval (
-        approval_id CHAR(1),
-        facility_id CHAR(1),
-        PRIMARY KEY (approval_id),
-        FOREIGN KEY (facility_id) REFERENCES Facility(facility_id)
-    );
-''')
-
-#Student Table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Student (
-        student_id INTEGER NOT NULL,
-        password VARCHAR(20),
-        first_name VARCHAR(15),
-        last_name VARCHAR(15),
-        class VARCHAR(3),
-        approval_id CHAR(1),
-        PRIMARY KEY (student_id),
-        FOREIGN KEY (approval_id) REFERENCES Approval(approval_id)
     );
 ''')
 
@@ -64,13 +31,38 @@ cursor.execute('''
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Booking (
         booking_number INTEGER,
-        facility_id CHAR(1),
-        student_id INTEGER,
+        facility_id INTEGER,
+        user_id VARCHAR(10),
         booking_start_time DATETIME,
         booking_end_time DATETIME,
+        approved BOOLEAN,
         PRIMARY KEY (booking_number),
         FOREIGN KEY (facility_id) REFERENCES Facility(facility_id),
-        FOREIGN KEY (student_id) REFERENCES Student(student_id)
+        FOREIGN KEY (user_id) REFERENCES User(user_id)
+    );
+''')
+
+#Card Table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Card (
+        card_id INTEGER,
+        user_id VARCHAR(10),
+        PRIMARY KEY (card_id),
+        FOREIGN KEY (user_id) REFERENCES User(user_id)
+    );
+''')
+
+#Swipe Table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Swipe (
+        swipe_number INTEGER,
+        card_id INTEGER,
+        facility_id INTEGER,
+        date_time DATETIME,
+        access_accepted BOOLEAN,
+        PRIMARY KEY (swipe_number)
+        FOREIGN KEY (card_id) REFERENCES Card(card_id)
+        FOREIGN KEY (facility_id) REFERENCES Facility(facility_id)
     );
 ''')
 
