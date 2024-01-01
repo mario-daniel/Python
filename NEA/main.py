c6121af7e9f5b90082713b1715e31e2f3dc7a5cc
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2024-01-01 20:10:44 +0400

    Update main.py

--- NEA/main.py
+++ NEA/main.py
@@ -26,34 +26,34 @@ class Student(User):
         timing_format = timing.get()
         for index in range(len(timing_format)):
             if timing_format[index] == '-':
-                start_time = f'{timing_format[:index - 1].strip()}:00'
-                end_time = f'{timing_format[index + 1:].strip()}:00'
+                self.start_time = f'{timing_format[:index - 1].strip()}:00'
+                self.end_time = f'{timing_format[index + 1:].strip()}:00'
         from datetime import datetime, timedelta
         # Get today's date
-        day = day.get()
+        self.day = day.get()
         today = datetime.today()
         Day_num = {'Monday' : 0, 'Tuesday' : 1, 'Wednesday' : 2, 'Thursday' : 3, 'Friday' : 4}
         # Calculate days until next Day
-        days_delta = (Day_num[day] - today.weekday()) % 7
+        days_delta = (Day_num[self.day] - today.weekday()) % 7
         # Calculate the date of the Day
-        date = (today + timedelta(days=days_delta)).strftime('%Y-%m-%d')
-        return date, start_time, end_time, day
+        self.date = (today + timedelta(days=days_delta)).strftime('%Y-%m-%d')
     
-    def request(self, facilities, day, facility, timing, timetable, timings_available_combobox):
-        date, start_time, end_time, day = self.get_time(day, timing)
-        cursor.execute('INSERT INTO Booking (facility_id, user_id, booking_start_time, booking_end_time, booking_date) VALUES (?, ?, ?, ?, ?)', (facilities[facility.get()], self.user_id, start_time, end_time, date))
+    def request(self, day, facility, timing, timings_available_combobox):
+        self.get_time(day, timing)
+        facility_id_db = cursor.execute('SELECT facility_id FROM Facility WHERE facility_name = ?', (facility.get(), )).fetchall()
+        self.timeslot_id_db = cursor.execute('SELECT timeslot_id FROM Timeslot WHERE day = ? AND facility_id = ? AND start_time = ? AND end_time = ?', (self.day, facility_id_db[0][0], self.start_time, self.end_time)).fetchall()
+        cursor.execute('INSERT INTO Booking (facility_id, user_id, timeslot_id, booking_date) VALUES (?, ?, ?, ?)', (facility_id_db[0][0], self.user_id, self.timeslot_id_db[0][0], self.date))
         conn.commit()
-        timetable = self.add_update_table(timetable, timing, facility, day)
+        self.selection = 'add'
+        self.update_table()
         timing.set('')
-        timings_available_combobox.config(state='disabled')
-        with open('timetable_on_server.py', 'w') as file:
-            file.write(timetable)
-        messagebox.showinfo('Request Successful', f'Requested from {start_time} to {end_time} on {day} {date}')
+        timings_available_combobox.config(state = 'disabled')
+        messagebox.showinfo('Request Successful', f'Requested from {self.start_time} to {self.end_time} on {self.day} {self.date}')
     
-    def add_update_table(self, timetable, timing, facility, day):
-        if timing.get() in timetable[facility.get()][day]:
-            timetable[facility.get()][day].remove(timing.get())
-        return timetable
+    def update_table(self):
+        if self.selection == 'add':
+            cursor.execute('UPDATE Timeslot SET status = NULL WHERE timeslot_id = ?', (self.timeslot_id_db[0][0],))
+            conn.commit()
 
 class Teacher(User):
     def __init__(self, first_name = '', last_name = '', user_id = '', hashed_password = '', salt = '', facility = 0):
@@ -189,7 +189,9 @@ def register_page():
         elif re.match(pattern, password.get()):
             password = password.get()
             hashed_password, salt = password_hash(password)
-            cursor.execute('INSERT INTO User (user_id, facility_id, first_name, last_name, hashed_password, salt, class_grade) VALUES (?, ?, ?, ?, ?, ?, ?)', (user_id.get(), facilities[facility.get()], first_name.get(), last_name.get(), hashed_password, salt, class_grade.get()))
+            facility_id_db = cursor.execute('SELECT facility_id FROM Facility WHERE facility_name = ?', (facility.get(), )).fetchall()
+            if facility_id_db == []: facility_id_db = [(0,)]
+            cursor.execute('INSERT INTO User (user_id, facility_id, first_name, last_name, hashed_password, salt, class_grade) VALUES (?, ?, ?, ?, ?, ?, ?)', (user_id.get(), facility_id_db[0][0], first_name.get(), last_name.get(), hashed_password, salt, class_grade.get()))
             conn.commit()
             messagebox.showinfo('Registration Successful', 'Please Login')
             login_page()
@@ -230,7 +232,7 @@ def register_page():
     class_grade = tk.StringVar()
     facility = tk.StringVar()
     class_facility_bool = tk.BooleanVar()
-    facilities = {'' : 0, 'Football': 1, 'Sixth Form Room': 2, 'Basketball': 3, 'Cricket': 4, 'Multi-Purpose Hall': 5, 'Fitness Suite': 6}
+    facilities = ('Football', 'Sixth Form Room', 'Basketball', 'Cricket', 'Multi-Purpose Hall', 'Fitness Suite')
     classes = ('9A', '9B', '9C', '9D', '10A', '10B', '10C', '10D', '11A', '11B', '11C', '11D', '12A', '12B', '12C', '12D', '13A', '13B', '13C', '13D')
 
     #Frames
@@ -244,7 +246,7 @@ def register_page():
     ttk.Label(main_frame, text = 'Enter User ID').pack()
     ttk.Entry(main_frame, textvariable = user_id).pack()
     ttk.Label(main_frame, text = 'Enter Password').pack()
-    ttk.Entry(main_frame, textvariable = password, show = '*').pack()
+    ttk.Entry(main_frame, textvariable = password).pack()
     ttk.Label(main_frame, text = 'Enter First Name').pack()
     ttk.Entry(main_frame, textvariable = first_name).pack()
     ttk.Label(main_frame, text = 'Enter Last Name').pack()
@@ -281,15 +283,16 @@ def home_page(user):
 
 def approval_request_page(user):
 
-    def display_timings_available(timing, timings_available_combobox, timetable):
+    def display_timings_available(day, facility, timings_available_combobox):
         timings_available = []
-        if facility.get() != '':
-            for facility_dict, days in timetable.items():
-                if facility_dict == facility.get():
-                    for day_dict, timings in days.items():
-                        if day_dict == day.get():
-                            for timing in timings:
-                                timings_available.append(timing)
+        timings = cursor.execute('''SELECT Timeslot.start_time, Timeslot.end_time 
+                                 FROM Timeslot 
+                                 JOIN Facility ON Facility.facility_id = Timeslot.facility_id 
+                                 WHERE Timeslot.day = ? 
+                                    AND Facility.facility_name = ? 
+                                    AND Timeslot.status = 0;''' ,(day.get(), facility.get())).fetchall()
+        for slot in timings:
+            timings_available.append(f'{slot[0][:-3]} - {slot[1][:-3]}')
         timings_available_combobox.config(state = 'active')
         timings_available_combobox.config(values = timings_available)
 
@@ -308,13 +311,11 @@ def approval_request_page(user):
     remove_widgets()
 
     #Variables
-    facilities = {'Football': 1, 'Sixth Form Room': 2, 'Basketball': 3, 'Cricket': 4, 'Multi-Purpose Hall': 5, 'Fitness Suite': 6}
+    facilities = ('Football', 'Basketball', 'Cricket', 'Multi-Purpose Hall', 'Fitness Suite')
     facility = tk.StringVar()
     day = tk.StringVar()
     timing = tk.StringVar()
-    from timetable_on_server import timetable
-    from original_timetable_on_server import original_timetable
-
+    
     #Frames
     main_frame = ttk.Frame(window, width = 900, height = 600)
     main_frame.pack(expand = True, fill = 'both')
@@ -331,8 +332,8 @@ def approval_request_page(user):
     ttk.Label(main_frame, text = 'Pick Timing').pack()
     timings_available_combobox = ttk.Combobox(main_frame, state = 'disabled', textvariable = timing, values = [])
     timings_available_combobox.pack()
-    ttk.Button(main_frame, text = 'Check Available Timings', command = lambda: display_timings_available(timing, timings_available_combobox, timetable)).pack()
-    ttk.Button(main_frame, text = 'Request', command = lambda: user.request(facilities, day, facility, timing, timetable, timings_available_combobox)).pack()
+    ttk.Button(main_frame, text = 'Check Available Timings', command = lambda: display_timings_available(day, facility, timings_available_combobox)).pack()
+    ttk.Button(main_frame, text = 'Request', command = lambda: user.request(day, facility, timing, timings_available_combobox)).pack()
     ttk.Button(main_frame, text = 'Check outgoing approvals', command = lambda: display_outgoing_approvals(user, outgoing_approval_frame)).pack()
     ttk.Button(main_frame, text = '<--- Home', command = lambda: home_page(user)).pack()
 
