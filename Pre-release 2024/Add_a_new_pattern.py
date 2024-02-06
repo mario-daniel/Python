c84b697295f3d712cedaf3bdbc8490dfafb5fb8b
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2024-02-06 09:27:30 +0400

    Create Add_a_new_pattern.py

---
+++ Pre-release 2024/Add_a_new_pattern.py
@@ -0,0 +1,37 @@
+class Puzzle():
+    def __init__(self, *args):
+        if len(args) == 1:
+            self.__Score = 0
+            self.__SymbolsLeft = 0
+            self.__GridSize = 0
+            self.__Grid = []
+            self.__AllowedPatterns = []
+            self.__AllowedSymbols = []
+            self.__LoadPuzzle(args[0])
+        else:
+            self.__Score = 0
+            self.__SymbolsLeft = args[1]
+            self.__GridSize = args[0]
+            self.__Grid = []
+            for Count in range(1, self.__GridSize * self.__GridSize + 1):
+                if random.randrange(1, 101) < 90:
+                    C = Cell()
+                else:
+                    C = BlockedCell()
+                self.__Grid.append(C)
+            self.__AllowedPatterns = []
+            self.__AllowedSymbols = []
+            QPattern = Pattern("Q", "QQ**Q**QQ")
+            self.__AllowedPatterns.append(QPattern)
+            self.__AllowedSymbols.append("Q")
+            XPattern = Pattern("X", "X*X*X*X*X")
+            self.__AllowedPatterns.append(XPattern)
+            self.__AllowedSymbols.append("X")
+            TPattern = Pattern("T", "TTT**T**T")
+            self.__AllowedPatterns.append(TPattern)
+            self.__AllowedSymbols.append("T")
+--------------------------------------------------------------------------------------------------------------------------
+            CPattern = Pattern("C", "CCC*CCCC*")
+            self.__AllowedPatterns.append(CPattern)
+            self.__AllowedSymbols.append("C")
+--------------------------------------------------------------------------------------------------------------------------
