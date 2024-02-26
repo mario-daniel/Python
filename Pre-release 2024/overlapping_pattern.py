9807ff6323326083fa1699e8e79fd5a6a94a6352
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2024-02-26 07:52:02 +0400

    Update overlapping_pattern.py

--- Pre-release 2024/overlapping_pattern.py
+++ Pre-release 2024/overlapping_pattern.py
@@ -126,7 +126,7 @@ class Puzzle():
     def CheckforMatchWithPattern(self, Row, Column):
         for StartRow in range(Row + 2, Row - 1, -1):
             for StartColumn in range(Column - 2, Column + 1):
-                #try:
+                try:
                     PatternString = ""
                     PatternString += self.__GetCell(StartRow, StartColumn).GetSymbol()
                     PatternString += self.__GetCell(StartRow, StartColumn + 1).GetSymbol()
@@ -141,21 +141,22 @@ class Puzzle():
                         CurrentSymbol = self.__GetCell(Row, Column).GetSymbol()
                         if P.MatchesPattern(PatternString, CurrentSymbol):
                             PatternString = ''.join(['*' if char != CurrentSymbol else char for char in PatternString])
-                            if PatternString == self.__AllowedPatterns:
-                                self.__GetCell(StartRow, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
-                                self.__GetCell(StartRow, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
-                                self.__GetCell(StartRow, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
-                                self.__GetCell(StartRow - 1, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
-                                self.__GetCell(StartRow - 2, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
-                                self.__GetCell(StartRow - 2, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
-                                self.__GetCell(StartRow - 2, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
-                                self.__GetCell(StartRow - 1, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
-                                self.__GetCell(StartRow - 1, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
-                                return 10
+                            for index in range(len(self.__AllowedPatterns)):
+                                if self.__AllowedPatterns[index].__PatternSequence == PatternString:
+                                    self.__GetCell(StartRow, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
+                                    self.__GetCell(StartRow, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
+                                    self.__GetCell(StartRow, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
+                                    self.__GetCell(StartRow - 1, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
+                                    self.__GetCell(StartRow - 2, StartColumn + 2).AddToNotAllowedSymbols(CurrentSymbol)
+                                    self.__GetCell(StartRow - 2, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
+                                    self.__GetCell(StartRow - 2, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
+                                    self.__GetCell(StartRow - 1, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
+                                    self.__GetCell(StartRow - 1, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
+                                    return 10
                             else:
-                                print('\nOverlapping pattern\n')
-                #except:
-                #   pass
+                                print('Overlapping Pattern')
+                except:
+                    pass
         return 0
 
     def __GetSymbolFromUser(self):
