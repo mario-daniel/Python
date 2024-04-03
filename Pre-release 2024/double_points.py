3b43978883f4a763b934a68d6e780a47f043295c
mario-daniel <44317448+mario-daniel@users.noreply.github.com>
2024-04-03 16:56:15 +0400

    Update double_points.py

--- Pre-release 2024/double_points.py
+++ Pre-release 2024/double_points.py
@@ -19,7 +19,7 @@ class Puzzle():
                 else:
                     C = BlockedCell()
                 self.__Grid.append(C)
-------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
             for Count in range(0, 5):
                 Index = random.randint(1, 63)
                 C = self.__Grid[Index]
@@ -27,7 +27,7 @@ class Puzzle():
                     Index = random.randint(1, 63)
                     C = self.__Grid[Index]
                 self.__Grid[Index] = DoublePointCell()
-------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
             self.__AllowedPatterns = []
             self.__AllowedSymbols = []
             QPattern = Pattern("Q", "QQ**Q**QQ")
@@ -44,7 +44,7 @@ class Puzzle():
             for StartColumn in range(Column - 2, Column + 1):
                 try:
                     PatternString = ""
-------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
                     Pattern = []
                     PatternString += self.__GetCell(StartRow, StartColumn).GetSymbol()
                     Pattern.append(self.__GetCell(StartRow, StartColumn))
@@ -64,7 +64,7 @@ class Puzzle():
                     Pattern.append(self.__GetCell(StartRow - 1, StartColumn))
                     PatternString += self.__GetCell(StartRow - 1, StartColumn + 1).GetSymbol()
                     Pattern.append(self.__GetCell(StartRow - 1, StartColumn))
-------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
                     for P in self.__AllowedPatterns:
                         CurrentSymbol = self.__GetCell(Row, Column).GetSymbol()
                         if P.MatchesPattern(PatternString, CurrentSymbol):
@@ -77,7 +77,7 @@ class Puzzle():
                             self.__GetCell(StartRow - 2, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
                             self.__GetCell(StartRow - 1, StartColumn).AddToNotAllowedSymbols(CurrentSymbol)
                             self.__GetCell(StartRow - 1, StartColumn + 1).AddToNotAllowedSymbols(CurrentSymbol)
-------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
                             for cell in Pattern:
                                 try:
                                     if cell.IsDouble():
@@ -86,14 +86,14 @@ class Puzzle():
                                 except:
                                         self.__Points = 10
                             return self.__Points
-------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
                 except:
                     pass
         return 0
             self.__AllowedPatterns.append(TPattern)
             self.__AllowedSymbols.append("T")
 
-------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
 class DoublePointCell(Cell):
     def __init__(self):
         super(DoublePointCell, self).__init__()
@@ -101,4 +101,4 @@ class DoublePointCell(Cell):
 
     def IsDouble(self):
         return True
-------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
